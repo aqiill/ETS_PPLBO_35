86c0a78afe0321070a7d9b6eeffa96df7a9379ce
Aqil Rahman <aqilrahman23@gmail.com>
2023-03-07 11:15:08 +0700

    service

---
+++ kafe-service/src/main/java/com/aqilrahman/kafeservice/KafeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aqilrahman.kafeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafeServiceApplication.class, args);
+	}
+
+}
